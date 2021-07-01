4bdf0693b6e9032299f3b50b1f05861f43e2c974
halbtale <halbtale@gmail.com>
2021-07-01 16:26:01 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+import Gun from 'gun/gun';
+const gun = new Gun({peers: ['https://gunjs-server.herokuapp.com/gun']});
+
+const nameDomElement = document.getElementById('name')
+const inputDomElement = document.getElementById("input")
+
+inputDomElement.addEventListener('input', () => {
+    /** @type {String} */
+    const name = inputDomElement.value;
+    gun.get('main').put({ name });
+})
+
+gun.get('main').on((data) => {
+    nameDomElement.innerText = data.name;
+    inputDomElement.value = data.name;
+})
